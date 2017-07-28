71ff18b75b17bf1da5a98e6a4823e7d70ce8faac
Tivel <tivel@idzaaus.org>
2017-07-29 00:01:23 +0300

    validates name not en

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }, exclusion: { in: ["en"],
+                                                                                                               message: "%{value} is reserved." }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
